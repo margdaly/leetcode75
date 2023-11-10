ecd07a5c8bab2ba4346b3c9c31d172e28c0097c6
margdaly <margaret.daly@gmail.com>
2023-11-09 17:55:36 -0700

    Add category headings to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,47 @@
 require 'rspec'
 require 'pry'
 
+# ARRAYS / STRING
 require './lib/arrays_strings/merge_alternately.rb'
+
+# TWO POINTERS
+
+# SLIDING WINDOW
+
+# PREFIX SUM
+
+# HASH MAP / SET
+
+# STACK
+
+# QUEUE
+
+# LINKED LIST
+
+# BINARY TREE - DFS
+
+# BINARY TREE - BFS
+
+# BINARY SEARCH TREE
+
+# GRAPHS - DFS
+
+# GRAPHS - BFS
+
+# HEAP / PRIORITY QUEUE
+
+# BINARY SEARCH
+
+# BACKTRACKING
+
+# DP - 1D
+
+# DP - MULTIDIMENSIONAL
+
+# BIT MANIPULATION
+
+# TRIE
+
+# INTERVALS
+
+# MONOTONIC STACK

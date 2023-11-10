b60e25398b7c8944670a3c03ddd3c20f59b4b82b
margdaly <margaret.daly@gmail.com>
2023-11-09 17:36:53 -0700

    test: Add tests for diff length or empty strings

---
+++ spec/arrays_strings_spec/merge_alternately_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe '#merge_alternately' do
+  it 'merges letters in alternating order, starting with word1' do
+    expect(merge_alternately('abc', 'xyz')).to eq('axbycz')
+    expect(merge_alternately('xxx', 'ooo')).to eq('xoxoxo')
+  end
+
+  it 'appends the additional letters if a word is longer than other' do
+    expect(merge_alternately('aa', 'bcus')).to eq('abacus')
+    expect(merge_alternately('jklmnop', 'rs')).to eq('jrlsmnopq')
+  end
+
+  it 'handles empty strings' do
+    expect(merge_alternately('', 'xyz')).to eq('xyz')
+    expect(merge_alternately('abc', '')).to eq('abc')
+  end
+end

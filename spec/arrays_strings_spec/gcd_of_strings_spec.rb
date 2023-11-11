9e93a56a8e890ca5255b6ef1032ddb3b3db8801a
margdaly <margaret.daly@gmail.com>
2023-11-11 13:03:23 -0700

    test: Update single test into 3 separate tests

--- spec/arrays_strings_spec/gcd_of_strings_spec.rb
+++ spec/arrays_strings_spec/gcd_of_strings_spec.rb
@@ -2,10 +2,24 @@ require 'spec_helper'
 
 # Greatest Common Divisor of Strings
 
+# For two strings s and t, we say "t divides s" 
+# if and only if s = t + ... + t 
+# (i.e., t is concatenated with itself one or more times).
+
 describe '#gcd_of_strings' do
-  it 'returns the largest string that divides both strings' do
-    expect(gcd_of_strings("ABCABC", "ABC")).to eq("ABC")
-    expect(gcd_of_strings("ABABAB", "ABAB")).to eq("AB")
-    expect(gcd_of_strings("LEET", "CODE")).to eq("")
+  context 'string1 is divisable by string2' do
+    it 'returns the largest string that divides both strings' do
+      expect(gcd_of_strings('ABABAB', 'ABAB')).to eq('AB')
+    end
+    
+    it 'returns string2 when string1 can only be divided once' do
+      expect(gcd_of_strings('ABCABC', 'ABC')).to eq('ABC')
+    end
+  end
+    
+  context 'no common divisor is found' do
+    it 'returns an empty string' do
+      expect(gcd_of_strings('LEET', 'CODE')).to eq('')
+    end
   end
 end
